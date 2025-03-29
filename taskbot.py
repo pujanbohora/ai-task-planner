c67545fbf5fd1e6aa3e7c26c5562956b0ffe4de9
Pujan Bohora <bohorapawan@gmail.com>
2025-03-28 23:12:20 -0500

    Ask input for user

---
+++ taskbot.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to TaskBot!")
+    task = input("what do you need help with?\n")
+    print("\n Here's your plan basen on your input:")
+    print(f"- Focus on: {task}")
+    print("Motivation: Keep going, you're doing great!")
+
+if __name__ == "__main__":
+    main()
